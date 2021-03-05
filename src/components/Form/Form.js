16c6767a5aaad1fface0846f69c8851dca604014
Shakiba448 <shakiba448@gmail.com>
2021-03-05 15:36:30 +0600

    WIP on function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,16 +1,35 @@
-import { Grid } from "@material-ui/core";
+import { Box, Grid } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { LimitedWordTextarea } from "../WordCountForm/WordCountForm";
 
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  items: {
+    padding: "10px",
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+  },
+});
+
 const Form = () => {
+  const classes = useStyles();
   return (
     <>
-      <Grid container justify="center">
-        <Grid item>
-          <LimitedWordTextarea limit={5} value="Hello there!" />
+      <Box className={classes.root}>
+        <Grid item className={classes.items}>
+          <LimitedWordTextarea />
+          <LimitedWordTextarea />
+          <LimitedWordTextarea />
+          <LimitedWordTextarea />
         </Grid>
-      </Grid>
+      </Box>
     </>
   );
 };
--- src/components/WordCountForm/WordCountForm.js
+++ src/components/WordCountForm/WordCountForm.js
@@ -1,6 +1,7 @@
+import { TextField } from "@material-ui/core";
 import React, { useCallback, useEffect, useState } from "react";
 
-export const LimitedWordTextarea = ({ rows, cols, value, limit }) => {
+export const LimitedWordTextarea = ({ value, limit }) => {
   const [{ content, wordCount }, setContent] = useState({
     content: value,
     wordCount: 0,
@@ -8,14 +9,14 @@ export const LimitedWordTextarea = ({ rows, cols, value, limit }) => {
 
   const setFormattedContent = useCallback(
     (text) => {
-      let words = text.split(" ").filter(Boolean);
-      if (words.length > limit) {
+      let words = text?.split(" ")?.filter(Boolean);
+      if (words?.length > limit) {
         setContent({
-          content: words.slice(0, limit).join(" "),
+          content: words?.slice(0, limit)?.join(","),
           wordCount: limit,
         });
       } else {
-        setContent({ content: text, wordCount: words.length });
+        setContent({ content: text, wordCount: words?.length });
       }
     },
     [limit, setContent]
@@ -27,15 +28,17 @@ export const LimitedWordTextarea = ({ rows, cols, value, limit }) => {
 
   return (
     <>
-      <textarea
-        rows={rows}
-        cols={cols}
+      <TextField
+        // inputProps={{
+        //   maxlength: limit,
+        // }}
         onChange={(event) => setFormattedContent(event.target.value)}
         value={content}
+        margin="normal"
+        variant="outlined"
+        // helperText={`${wordCount}/${limit}`}
+        helperText={`${wordCount === undefined ? 0 : wordCount}`}
       />
-      <p>
-        {wordCount}/{limit}
-      </p>
     </>
   );
 };
