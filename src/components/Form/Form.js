441154a595520196d24208fa3d8de454ca7ec859
Shakiba448 <shakiba448@gmail.com>
2021-03-05 19:42:43 +0600

    WIP on function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,7 +1,6 @@
-import { Box, Grid } from "@material-ui/core";
+import { Box, Container, Grid, FormGroup, TextField } from "@material-ui/core";
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { LimitedWordTextarea } from "../WordCountForm/WordCountForm";
 
 const useStyles = makeStyles({
   root: {
@@ -16,19 +15,57 @@ const useStyles = makeStyles({
     flexDirection: "column",
     flexWrap: "wrap",
   },
+  formControl: {
+    marginBottom: "15px",
+  },
 });
 
 const Form = () => {
   const classes = useStyles();
+
   return (
     <>
       <Box className={classes.root}>
-        <Grid item className={classes.items}>
-          <LimitedWordTextarea />
-          <LimitedWordTextarea />
-          <LimitedWordTextarea />
-          <LimitedWordTextarea />
-        </Grid>
+        <Container>
+          <Grid container justify="center">
+            <Grid item md={6} sm={12} lg={6} className={classes.items}>
+              <FormGroup>
+                <TextField
+                  id="filled-basic"
+                  label="Count of joker words with first 70 words"
+                  variant="filled"
+                  className={classes.formControl}
+                />
+                <TextField
+                  id="filled-basic"
+                  label="What You Get 3 sec. 7 words"
+                  variant="filled"
+                  className={classes.formControl}
+                />
+                <TextField
+                  id="filled-basic"
+                  label="Why You?"
+                  variant="filled"
+                  className={classes.formControl}
+                />
+                <TextField
+                  id="filled-basic"
+                  label="Conclusion 7 sec. 17 words"
+                  variant="filled"
+                  className={classes.formControl}
+                />
+                <TextField
+                  label="Explanation"
+                  multiline
+                  rows={4}
+                  rowsMax={8}
+                  variant="filled"
+                  className={classes.formControl}
+                />
+              </FormGroup>
+            </Grid>
+          </Grid>
+        </Container>
       </Box>
     </>
   );
