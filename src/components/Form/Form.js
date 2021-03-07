eb1235d190d9de71cf60369808d6a196544bb505
Shakiba448 <shakiba448@gmail.com>
2021-03-07 15:21:05 +0600

    WIP on function

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -14,13 +14,16 @@ import { useState } from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { lengthCount, textData } from "../../Utils/Utils";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     margin: "0 25px",
+    [theme.breakpoints.down("sm")]: {
+      marginTop: "200px",
+    },
   },
   formControl: {
     marginBottom: "15px",
@@ -31,7 +34,15 @@ const useStyles = makeStyles({
   copyBtn: {
     position: "absolute",
     top: 405,
-    right: 270,
+    left: 0,
+    right: 130,
+    marginLeft: "auto",
+    marginRight: "auto",
+    [theme.breakpoints.down("sm")]: {
+      top: 405,
+      left: 0,
+      right: 0,
+    },
   },
   countItems: {
     fontSize: 25,
@@ -45,7 +56,14 @@ const useStyles = makeStyles({
   countItemsNotOk: {
     color: "red !important",
   },
-});
+  countSection: {
+    [theme.breakpoints.down("sm")]: {
+      display: "flex",
+      justifyContent: "center",
+      marginBottom: 25,
+    },
+  },
+}));
 
 const Form = () => {
   const classes = useStyles();
@@ -196,50 +214,53 @@ const Form = () => {
       <Container>
         <Grid container direction="row" justify="center" spacing={8}>
           <Grid item md={6} sm={12} lg={6} xs={12}>
-            <FormGroup>
-              <TextField
-                id="filled-basic"
-                label="Count of joker words with first 70 words"
-                variant="outlined"
-                className={classes.formControl}
-                name="joker"
-                onChange={(e) => wordCount(e.target.value, "joker")}
-              />
-              <TextField
-                id="filled-basic"
-                label="What You Get 3 sec.7 words"
-                variant="outlined"
-                className={classes.formControl}
-                name="get"
-                onChange={(e) => wordCount(e.target.value, "get")}
-              />
-              <TextField
-                id="filled-basic"
-                label="Why You? 5 sec. 12 words"
-                variant="outlined"
-                name="why"
-                className={classes.formControl}
-                onChange={(e) => wordCount(e.target.value, "why")}
-              />
-              <TextField
-                id="filled-basic"
-                label="Conclusion 7 sec. 17 words"
-                variant="outlined"
-                name="conclusion"
-                className={classes.formControl}
-                onChange={(e) => wordCount(e.target.value, "conclusion")}
-              />
-              <TextField
-                label="Explanation"
-                multiline
-                rows={4}
-                rowsMax={8}
-                variant="outlined"
-                name="explanation"
-                className={classes.formControl}
-                onChange={(e) => wordCount(e.target.value, "explanation")}
-              />
-            </FormGroup>
+            <form>
+              <FormGroup>
+                <TextField
+                  id="outlined-basic"
+                  label="Count of joker words with first 70 words"
+                  variant="outlined"
+                  className={classes.formControl}
+                  name="joker"
+                  onChange={(e) => wordCount(e.target.value, "joker")}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="What You Get 3 sec.7 words"
+                  variant="outlined"
+                  className={classes.formControl}
+                  name="get"
+                  onChange={(e) => wordCount(e.target.value, "get")}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Why You? 5 sec. 12 words"
+                  variant="outlined"
+                  name="why"
+                  className={classes.formControl}
+                  onChange={(e) => wordCount(e.target.value, "why")}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Conclusion 7 sec. 17 words"
+                  variant="outlined"
+                  name="conclusion"
+                  className={classes.formControl}
+                  onChange={(e) => wordCount(e.target.value, "conclusion")}
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Explanation"
+                  multiline
+                  rows={4}
+                  rowsMax={8}
+                  variant="outlined"
+                  name="explanation"
+                  className={classes.formControl}
+                  onChange={(e) => wordCount(e.target.value, "explanation")}
+                />
+              </FormGroup>
+            </form>
           </Grid>
           <Grid
             container
@@ -248,7 +269,7 @@ const Form = () => {
             lg={4}
             xs={12}
             spacing={4}
-            className={classes.countArea}
+            className={clsx(classes.countArea, classes.countSection)}
           >
             <Grid item>
               <Typography variant="h5">Word Count</Typography>
